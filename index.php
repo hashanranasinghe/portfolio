6484ce43479c7d71ec2f4881d2389667e3af5f0e
hashanranasinghe <hashan.ranasinghe98@gmail.com>
2022-06-26 18:32:48 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,143 @@
+<?php
+   include_once('navBar.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class=" col-md-6 justify-content-center">
+                    <div class="container-fluid pt-5">
+                        <i class="fa-brands fa-facebook fa-2xl pl-5 pt-5" id="icon"></i>
+                        <i class="fa-brands fa-instagram fa-2xl pl-3" id="icon"></i>
+                        <i class="fa-brands fa-linkedin fa-2xl pl-3" id="icon"></i>
+                        <i class="fa-brands fa-whatsapp fa-2xl pl-3" id="icon"></i>
+                        <i class="fa-brands fa-github fa-2xl pl-3 pr-5" id="icon"></i>
+                    </div>
+                    <h1 class=" text-light pl-5 pt-4">I’m Hashan Ranasinghe</h1>
+                    <p class="lead text-light pl-5 pt-1 text-justify">I'm Hashan, professional mobile application developer with long time experience in this field.</p>
+        
+            </div>
+            <div class = "col-md-6">
+                <div class = "container-fluid justify-content-center">
+                    <img src="img/one.png" class = "pl-5 pt-3" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+        <div class="container text-md-center">
+            <h1 class=" text-light">MY SERVICES</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-one.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Development</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-two.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Design</h5>
+                        <p class="card-text">This is a short card.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-three.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Web Design</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container text-md-center">
+            <h1 class=" text-light">MY PROJECTS</h1>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-one.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Development</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-two.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Design</h5>
+                        <p class="card-text">This is a short card.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-three.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Web Design</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-one.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Development</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-two.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">App Design</h5>
+                        <p class="card-text">This is a short card.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col pb-3">
+                <div class="card h-100">
+                    <img src="img/crd-three.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Web Design</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container text-md-center">
+            <h1 class=" text-light">ABOUT ME</h1>
+            <p class=" text-light text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, officiis nostrum aperiam ipsum quasi assumenda asperiores! Commodi facere officia nisi voluptates provident magnam autem accusantium. Aliquid corporis cupiditate voluptatum ea? Earum ad, quia et debitis aliquid quasi quod aspernatur voluptate aperiam incidunt! Laudantium, repellat. Natus aliquam optio aut vitae provident.</p>
+
+        </div>
+    </div>
+        
+    
+    
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ navBar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="custom.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm font-weight-bold">
+        <a href="" class="navbar-brand ml-5">H</a>
+        <button class=" navbar-toggler" data-toggle="collapse" data-target="#first"><span class=" navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse justify-content-end" id="first">
+            <ul class=" navbar-nav">
+                <li class="nav-item pr-5"><a href="" class="nav-link active">Home</a> </li>
+                <li class="nav-item pr-5"><a href="" class=" nav-link">Services</a> </li>
+                <li class="nav-item pr-5"><a href="" class=" nav-link">About</a> </li>
+                <li class="nav-item pr-5"><a href="" class=" nav-link">Projects</a> </li>
+                <li class="nav-item pr-5"><a href="" class=" nav-link">Contact</a> </li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+</html>
