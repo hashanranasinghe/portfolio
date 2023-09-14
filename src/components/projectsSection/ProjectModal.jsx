46308b69e1ea8a9a163772db09471439c977af42
Hashan Ranasinghe <73054932+hashanranasinghe@users.noreply.github.com>
2023-09-14 22:41:10 +0530

    dialog

--- src/components/projectsSection/ProjectModal.jsx
+++ src/components/projectsSection/ProjectModal.jsx
@@ -4,14 +4,14 @@ function ProjectModal({visible,onClose,project}) {
   return (
     <>
     <div
-      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+      className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none md:px-10 sm:px-10"
     >
       <div className="relative w-auto my-6 mx-auto max-w-3xl">
         {/*content*/}
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
           {/*header*/}
           <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-            <h3 className="text-3xl font-semibold">
+            <h3 className="xl:text-3xl font-semibold sm:text-sm">
               {project.name}
             </h3>
             <button
@@ -25,7 +25,7 @@ function ProjectModal({visible,onClose,project}) {
           </div>
           {/*body*/}
           <div className="relative p-6 flex-auto">
-            <p className="my-4 text-slate-500 text-lg leading-relaxed">
+            <p className="my-4 text-slate-500 xl:text-lg leading-relaxed sm:text-sm">
               {project.description}
             </p>
           </div>
